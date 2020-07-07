5f0a11a464c6d2c6e8c7d32b07f6611e8a6aae84
JB Ladera <32505118+MiniDomo@users.noreply.github.com>
2020-07-06 22:57:33 -0500

    correctly wait for dom using xpath

--- index.js
+++ index.js
@@ -1,27 +1,7 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 const auth = require('./auth.json');
-
-(async () => {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await login(page, auth);
-
-    await page.goto('https://www.hackerrank.com/contests/2020-pshs-adv-pokemasters/judge/submissions/1');
-    await page.waitForSelector('.span2.submissions-title');
-    await page.waitForSelector('.span1.submissions-title');
-    await page.waitForSelector('.span3.submissions-title');
-    const content = await page.content();
-    const $ = cheerio.load(content);
-    const arr = $('p', '.judge-submissions-list-view')
-        .map(function (i, e) {
-            return $(this).text().trim().replace(/\n.+/g, '').trim();
-        }).get();
-    console.log(arr);
-    console.log(arr.length);
-
-    console.log('done');
-})().catch(console.error);
+const fs = require('fs');
 
 const login = async (page, auth) => {
     await page.goto('https://www.hackerrank.com/auth/login');
@@ -39,3 +19,44 @@ const pause = async (ms) => {
         setTimeout(res, ms);
     });
 };
+
+const waitForElements = async (page) => {
+    try {
+        for (let x = 1; x <= 10; x++) {
+            await page.waitForXPath(`//*[@id="content"]/div/section/div/div/div/div[1]/div[${x}]/div/div[1]/p/a`); // problem
+            await page.waitForXPath(`//*[@id="content"]/div/section/div/div/div/div[1]/div[${x}]/div/div[2]/p/a`); // team
+            await page.waitForXPath(`//*[@id="content"]/div/section/div/div/div/div[1]/div[${x}]/div/div[3]/p`); // id
+            await page.waitForXPath(`//*[@id="content"]/div/section/div/div/div/div[1]/div[${x}]/div/div[4]/p`); // language
+            await page.waitForXPath(`//*[@id="content"]/div/section/div/div/div/div[1]/div[${x}]/div/div[5]/p`); // time
+            await page.waitForXPath(`//*[@id="content"]/div/section/div/div/div/div[1]/div[${x}]/div/div[6]/p`); // result
+            await page.waitForXPath(`//*[@id="content"]/div/section/div/div/div/div[1]/div[${x}]/div/div[7]/p`); // score
+            await page.waitForXPath(`//*[@id="content"]/div/section/div/div/div/div[1]/div[${x}]/div/div[8]/input`); // status
+            await page.waitForXPath(`//*[@id="content"]/div/section/div/div/div/div[1]/div[${x}]/div/div[9]/p`); // during contest
+            await page.waitForXPath(`//*[@id="content"]/div/section/div/div/div/div[1]/div[${x}]/div/div[10]/p/a`); // view
+        }
+        return true;
+    } catch (err) {
+        return false;
+    }
+};
+
+(async () => {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    page.setDefaultTimeout(5000);
+    await login(page, auth);
+
+    await page.goto('https://www.hackerrank.com/contests/2020-pshs-adv-pokemasters/judge/submissions/1');
+    await waitForElements(page);
+    const content = await page.content();
+    // fs.writeFileSync('./test5.html', content);
+    const $ = cheerio.load(content);
+    const arr = $('p', '.judge-submissions-list-view')
+        .map(function (i, e) {
+            return $(this).text().trim();
+        }).get();
+    console.log(arr);
+    console.log(arr.length);
+
+    console.log('done');
+})().catch(console.error);
